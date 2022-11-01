71931e1932eb6e1b6c4c2a2b1c3e314032f9d4ec
Todd Albert <todd@bocacode.com>
2022-11-01 17:09:41 -0400

    updated post to return all albums

--- functions/src/albums.js
+++ functions/src/albums.js
@@ -5,16 +5,17 @@ export function getAllAlbums(req, res) {
   db.collection('albums').get()
     .then(collection => {
       const albumsArr = collection.docs.map(doc => {
-        return {...doc.data(), albumId: doc.id }
+        return { ...doc.data(), albumId: doc.id }
       })
       res.send(albumsArr)
     })
     .catch(err => res.status(500).send({ success: false, message: err }))
-  }
-  
-  export function createNewAlbum(req, res) {
-    const db = dbConnect()
-    db.collection('albums').add(req.body)
-    .then(doc => res.status(201).send({ success: true, message: 'Album created: ' + doc.id }))
+}
+
+export function createNewAlbum(req, res) {
+  const db = dbConnect()
+  db.collection('albums').add(req.body)
+    .then(() => getAllAlbums(req, res))
+    // .then(doc => res.status(201).send({ success: true, message: 'Album created: ' + doc.id }))
     .catch(err => res.status(500).send({ success: false, message: err }))
 }
